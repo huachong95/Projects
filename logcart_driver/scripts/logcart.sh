30448fdb735455f6736201245ba88123d3f924a4
HuaChong <huachong95@hotmail.sg>
2020-06-17 01:05:40 +0800

    Rename source in Logcart script for dual-Odroid/PC implementation

--- logcart_driver/scripts/logcart.sh
+++ logcart_driver/scripts/logcart.sh
@@ -2,4 +2,4 @@
 sudo xboxdrv
 sudo bash -c "chmod a+rw /dev/input/js1"
 
-sudo bash -c "source /opt/ros/kinetic/setup.bash && source /home/huachong/catkin_ws/devel/setup.bash && rosclean purge -y && roslaunch logcart_driver logcart_teleop.launch"
+sudo bash -c "source /opt/ros/kinetic/setup.bash && source /catkin_ws/devel/setup.bash && rosclean purge -y && roslaunch logcart_driver logcart_teleop.launch"
