d6aa15922fc1683d7e13f3a27be995a82a616b35
HuaChong <huachong95@hotmail.sg>
2020-06-18 14:17:52 +0800

    source ~/catkin

--- logcart_mapping/scripts/mapping.sh
+++ logcart_mapping/scripts/mapping.sh
@@ -1,3 +1,3 @@
-sudo xboxdrv --detach-kernel-driver & sudo bash -c "chmod a+rw /dev/input/js0"
+source /catkisudo xboxdrv --detach-kernel-driver & sudo bash -c "chmod a+rw /dev/input/js0"
 
-sudo bash -c "source /opt/ros/kinetic/setup.bash && source /catkin_ws/devel/setup.bsah && rosclean purge -y && roslaunch logcart_driver logcart_teleop.launch" & source ~/catkin_ws/devel/setup.bash & roslaunch freenect_launch freenect.launch  & roslaunch logcart_mapping logcart_mapping.launch & roslaunch logcart_mapping pointcloudtolaserscan.launch
+sudo bash -c "source /opt/ros/kinetic/setup.bash && source ~/catkin_ws/devel/setup.bsah && rosclean purge -y && roslaunch logcart_driver logcart_teleop.launch" & source ~/catkin_ws/devel/setup.bash & roslaunch freenect_launch freenect.launch  & roslaunch logcart_mapping logcart_mapping.launch & roslaunch logcart_mapping pointcloudtolaserscan.launch
