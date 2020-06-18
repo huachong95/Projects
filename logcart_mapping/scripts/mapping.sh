90e333df91a85d5ee947402d829d2a26f616b3df
HuaChong <huachong95@hotmail.sg>
2020-06-18 13:49:31 +0800

    fixed freenect_launch error

--- logcart_mapping/scripts/mapping.sh
+++ logcart_mapping/scripts/mapping.sh
@@ -1,3 +1,3 @@
 sudo xboxdrv --detach-kernel-driver & sudo bash -c "chmod a+rw /dev/input/js0"
 
-sudo bash -c "source /opt/ros/kinetic/setup.bash && source ~/catkin_ws/devel/setup.bash && rosclean purge -y && roslaunch logcart_driver logcart_teleop.launch" & source ~/catkin_ws/devel/setup.bash & roslaunch freenect_launch & roslaunch logcart_driver logcart.sh & roslaunch logcart_mapping logcart_mapping.launch & roslaunch logcart_mapping pointcloudtolaserscan.launch
+sudo bash -c "source /opt/ros/kinetic/setup.bash && source ~/catkin_ws/devel/setup.bash && rosclean purge -y && roslaunch logcart_driver logcart_teleop.launch" & source ~/catkin_ws/devel/setup.bash & roslaunch freenect_launch freenect.launch & roslaunch logcart_driver logcart.sh & roslaunch logcart_mapping logcart_mapping.launch & roslaunch logcart_mapping pointcloudtolaserscan.launch
